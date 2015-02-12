78a91849cd5208fb5acbd7c0c27b792f4d730717
untrots <untrots@gmail.com>
2015-02-12 03:24:40 +0200

    main_ar: Wiring digital outputs

--- projects/main_ar/main_ar.ino
+++ projects/main_ar/main_ar.ino
@@ -3,9 +3,11 @@
 
 int numPwmChannels = 6;
 int pwmChannels[] = {3, 5, 6, 9, 10, 11};
-LedChannels ledChannels(numPwmChannels, pwmChannels);
+int numDigitalChannels = 6;
+int digitalChannels[] = {2, 4, 7, 8, 12, 13};
+LedChannels ledChannels(numPwmChannels, pwmChannels, numDigitalChannels, digitalChannels);
 
-SnarkLeds leds(ledChannels, 1, 3);
+SnarkLeds leds(ledChannels, 2, 6);
 
 void setup() {
   ledChannels.init();
@@ -14,17 +16,24 @@ void setup() {
 int f1 = 0;
 int f2 = 0;
 int f3 = 0;
+int f4 = 0;
 
 void loop() {
+  leds.setRgbColor(0, func(f1), func(f2), func(f3));
+  leds.setRgbColor(1, func(f2), func(f3), func(f4));
+  
   leds.setMonoColor(0, func(f1));
   leds.setMonoColor(1, func(f2));
   leds.setMonoColor(2, func(f3));
-  leds.setRgbColor(0, func(f1), func(f2), func(f3));
+  leds.setMonoColor(3, func(f4));
+  leds.setMonoColor(4, func(f3));
+  leds.setMonoColor(5, func(f2));
   
   f1 = (f1 + 1) % 512;
   f2 = (f2 + 3) % 512;
   f3 = (f3 + 5) % 512;
-
+  f4 = (f4 + 7) % 512;
+  
   delay(10);
 }
 

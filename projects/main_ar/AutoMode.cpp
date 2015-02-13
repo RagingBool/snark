7b5dc87b7d0848d821fa658119aef6a4161dc496
untrots <untrots@gmail.com>
2015-02-13 16:11:31 +0200

    AutoMode: creating AutoLed infrastructure; implementing FadingLed

--- projects/main_ar/AutoMode.cpp
+++ projects/main_ar/AutoMode.cpp
@@ -2,38 +2,39 @@
 
 AutoMode::AutoMode(LedManager& ledManager):
   _ledManager(ledManager) {
-  _f1 = 0;
-  _f2 = 10;
-  _f3 = 20;
-  _f4 = 30;
+  
+  _numAutoLeds = ledManager.getNumChannels();
+  
+  _pAutoLeds = new AutoLed*[_numAutoLeds];
+  for (int i = 0; i < _numAutoLeds; i++) {
+    _pAutoLeds[i] = new FadingLed(ledManager, i, i + 2, i * 11);
+  }
+}
+
+AutoMode::~AutoMode() {
+  for (int i = 0; i < _numAutoLeds; i++) {
+    delete _pAutoLeds[i];
+    _pAutoLeds[i] = 0;
+  }
+  
+  delete[] _pAutoLeds;
+  _pAutoLeds = 0;
 }
   
 void AutoMode::update() {
-  _ledManager.setValue(0, func(_f1));
-  _ledManager.setValue(1, func(_f2));
-  _ledManager.setValue(2, func(_f3));
-  _ledManager.setValue(3, func(_f2));
-  _ledManager.setValue(4, func(_f3));
-  _ledManager.setValue(5, func(_f4));
-  
-  _ledManager.setValue(6, func(_f1));
-  _ledManager.setValue(7, func(_f2));
-  _ledManager.setValue(8, func(_f3));
-  _ledManager.setValue(9, func(_f4));
-  _ledManager.setValue(10, func(_f3));
-  _ledManager.setValue(11, func(_f2));
-  
-  _f1 = (_f1 + 1) % 512;
-  _f2 = (_f2 + 3) % 512;
-  _f3 = (_f3 + 5) % 512;
-  _f4 = (_f4 + 7) % 512;
+  for (int i = 0; i < _numAutoLeds; i++) {
+    _pAutoLeds[i] -> update();
+  }
 }
 
-int AutoMode::func(int x) {
-  if(x < 256) {
-    return x;
-  }
-  else {
-    return 511 - x;
-  }
+// class FadingLed
+
+void AutoMode::FadingLed::update() {
+  int phase = _phase;
+  
+  int value = (phase < 256) ? phase : 511 - phase;
+  
+  setValue(value);
+  
+  _phase = (phase + _step) % 512;
 }
--- projects/main_ar/AutoMode.h
+++ projects/main_ar/AutoMode.h
@@ -3,19 +3,57 @@
 #include "LedManager.h"
 
 class AutoMode {
+  class AutoLed;
+  
 private:
   LedManager& _ledManager;
-
-  int _f1;
-  int _f2;
-  int _f3;
-  int _f4;
+  
+  int _numAutoLeds;
+  AutoMode::AutoLed** _pAutoLeds;
 
 public:
   AutoMode(LedManager& ledManager);
   
+  ~AutoMode();
+  
   void update();
-
+  
 private:
-  int func(int x);
+  class AutoLed {
+  private:
+    LedManager& _ledManager;
+      
+  public:
+    AutoLed(LedManager& ledManager): _ledManager(ledManager) { }
+    
+    virtual ~AutoLed() { }
+    
+    virtual void update() = 0;
+      
+  protected:
+    LedManager& getLedManager() { return _ledManager; }
+  };
+  
+  class AutoMonoLed: public AutoLed {
+  private:
+    int _ledId;
+    
+  public:
+    AutoMonoLed(LedManager& ledManager, int ledId): AutoLed(ledManager), _ledId(ledId) { }
+    
+  protected:
+    void setValue(int value) { getLedManager().setValue(_ledId, value); }
+  };
+  
+  class FadingLed: public AutoMonoLed {
+  private:
+    int _step;
+    int _phase;
+    
+  public:
+    FadingLed(LedManager& ledManager, int ledId, int step, int startingPhase = 0):
+      AutoMonoLed(ledManager, ledId), _step(step), _phase(startingPhase) { }
+    
+    virtual void update();
+  };
 };

93c528754580855938155982fc3d3370f0344a67
untrots <untrots@gmail.com>
2015-02-13 14:08:13 +0200

    +Snark

--- projects/main_ar/main_ar.ino
+++ projects/main_ar/main_ar.ino
@@ -1,54 +1,12 @@
-#include "Outputs.h"
-#include "LedManager.h"
+#include "Snark.h"
 
-int numPwmChannels = 6;
-int pwmChannels[] = {3, 5, 6, 9, 10, 11};
-int numDigitalChannels = 6;
-int digitalChannels[] = {2, 4, 7, 8, 12, 13};
-Outputs outputs(numPwmChannels, pwmChannels, numDigitalChannels, digitalChannels);
-
-LedManager leds(outputs);
+Snark snark;
 
 void setup() {
-  outputs.init();
+  snark.init();
 }
-
-int f1 = 0;
-int f2 = 10;
-int f3 = 20;
-int f4 = 30;
 
 void loop() {
-  leds.setValue(0, func(f1));
-  leds.setValue(1, func(f2));
-  leds.setValue(2, func(f3));
-  leds.setValue(3, func(f2));
-  leds.setValue(4, func(f3));
-  leds.setValue(5, func(f4));
-  
-  leds.setValue(6, func(f1));
-  leds.setValue(7, func(f2));
-  leds.setValue(8, func(f3));
-  leds.setValue(9, func(f4));
-  leds.setValue(10, func(f3));
-  leds.setValue(11, func(f2));
-  
-  f1 = (f1 + 1) % 512;
-  f2 = (f2 + 3) % 512;
-  f3 = (f3 + 5) % 512;
-  f4 = (f4 + 7) % 512;
-  
-  delay(10);
+  snark.loop();
 }
 
-int func(int x)
-{
-  if(x < 256)
-  {
-    return x;
-  }
-  else
-  {
-    return 511 - x;
-  }
-}

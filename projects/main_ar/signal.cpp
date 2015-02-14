3fb730e35b13d75695c4ed17d349abc134f09cd3
untrots <untrots@gmail.com>
2015-02-14 17:03:26 +0200

    PhaseGenerator: uses frequency instead of period; +Lfo

--- projects/main_ar/signal.cpp
+++ projects/main_ar/signal.cpp
@@ -1,1 +1,8 @@
 #include "signal.h"
+
+void Lfo::update(uint16_t deltaMillis) {
+  _phaseGenerator.update(deltaMillis);
+  
+  int phase = _phaseGenerator.getPhase();
+  _value = (phase < 256) ? phase : 511 - phase;
+}
--- projects/main_ar/signal.h
+++ projects/main_ar/signal.h
@@ -2,27 +2,52 @@
 
 #include <stdint.h>
 
+inline uint16_t periodToFrequency(uint16_t periodMillis) { return 0xFFFF / periodMillis; }
+
 class PhaseGenerator {
 private:
   int8_t _fixedPointBits;
   uint16_t _phaseFixed;
-  uint16_t _stepPerMillisecondFixed;
-  uint16_t _periodMillis;
+  uint16_t _frequency;
 
 public:
   PhaseGenerator(int8_t phaseResolutionBits):
     _fixedPointBits(16 - phaseResolutionBits),
-    _phaseFixed(0) { setPeriodMillis(1000); }
+    _phaseFixed(0) { setFrequency(1); }
   
-  uint16_t getPeriodMillis() const { return _periodMillis; }
-  void setPeriodMillis(uint16_t periodMillis) { _periodMillis = periodMillis; _stepPerMillisecondFixed = 0xFFFF / periodMillis; }
+  uint16_t getFrequency() const { return _frequency; }
+  void setFrequency(uint16_t frequency) { _frequency = frequency; }
   
   uint16_t getPhase() const { return fixedPhaseToPhase(_phaseFixed); }
   void setPhase(uint16_t phase) { _phaseFixed = phaseToFixedPhase(phase); }
   
-  void update(uint16_t deltaMillis) { _phaseFixed += deltaMillis * _stepPerMillisecondFixed; }
+  void update(uint16_t deltaMillis) { _phaseFixed += deltaMillis * _frequency; }
   
 private:
   uint16_t fixedPhaseToPhase(uint16_t fixedPhase) const { return fixedPhase >> _fixedPointBits; }
   uint16_t phaseToFixedPhase(uint16_t phase) const { return phase << _fixedPointBits; }
 };
+
+class Lfo {
+private:
+  PhaseGenerator _phaseGenerator;
+  uint16_t _frequency;
+  uint8_t _value;
+  uint8_t _initialPhase;
+  
+public:
+  Lfo(): _phaseGenerator(9), _value(0) { setFrequency(1); }
+  
+  uint16_t getFrequency() const { return _phaseGenerator.getFrequency(); }
+  void setFrequency(uint16_t frequency) { _phaseGenerator.setFrequency(frequency); }
+  
+  uint8_t getInitialPhase() const { return _initialPhase; }
+  void setInitialPhase(uint8_t initialPhase) { _initialPhase = initialPhase; }
+  
+  void reset() { _phaseGenerator.setPhase(_initialPhase * 2); }
+  
+  uint8_t getValue() const { return _value; }
+  
+  void update(uint16_t deltaMillis);
+};
+
